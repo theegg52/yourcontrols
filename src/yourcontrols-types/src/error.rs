4ef4b524940d5dc8cc81823972ded00fe61e4cd4
Sequal32 <connor0530@gmail.com>
2021-03-24 23:23:40 -0400

    ✅ ✨ working scripting engine, fix watcher tests

--- src/yourcontrols-types/src/error.rs
+++ src/yourcontrols-types/src/error.rs
@@ -36,6 +36,7 @@ pub enum Error {
     VariableInitializeError,
     // Gauge Scripting
     RhaiParse(rhai::ParseError),
+    RhaiError(Box<rhai::EvalAltResult>),
 
     // Misc
     None,
@@ -82,6 +83,7 @@ impl Display for Error {
             }
             Error::VariableInitializeError => write!(f, "Var could not be initialized."),
             Error::RhaiParse(e) => write!(f, "Could not parse RHAI script: {}", e),
+            Error::RhaiError(e) => write!(f, "Could not run RHAI script: {}", e),
             Error::None => write!(f, "No value returned."),
             Error::NotProcessed => write!(f, "Not processed."),
         }
@@ -121,5 +123,11 @@ impl From<crossbeam_channel::TryRecvError> for Error {
 impl From<rhai::ParseError> for Error {
     fn from(e: rhai::ParseError) -> Self {
         Error::RhaiParse(e)
+    }
+}
+
+impl From<Box<rhai::EvalAltResult>> for Error {
+    fn from(e: Box<rhai::EvalAltResult>) -> Self {
+        Error::RhaiError(e)
     }
 }
